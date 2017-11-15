f5344b928fa736350a0386112f09812736c0306b
roly <orlanmarti@gmail.com>
2017-11-15 13:43:54 +0100

    load initial data in fixtures when startup

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -1,19 +1,72 @@
-import AudiencesCategories from "/imports/api/audiencesCategories/audiencesCategories.js";
+import { AudienceCategories } from "/imports/api/audienceCategories/audienceCategories.js";
+import { Geolocations } from "/imports/api/geolocations/geolocations.js";
+import { Contexts } from "/imports/api/contexts/contexts.js";
+import _ from "underscore";
 
-const audiences = [
+const locations = [
   {
-    title: "Anarchism",
-    spec: { interests: ["6003029947985"] }
+    name: "São Paulo, Brazil",
+    facebookId: "112047398814697",
+    geoId: "08"
   },
   {
-    title: "Anarchism",
-    spec: { interests: ["6003029947985"] }
+    name: "Rio de Janeiro",
+    facebookId: "108154142551615",
+    geoId: "09"
   }
 ];
 
-if (AudiencesCategories.find().count() == 0) {
+if (Geolocations.find().count() == 0) {
+  for (const location of locations) {
+    Geolocations.insert(location);
+  }
+  logger.debug(
+    "fixtures. audiences locations added:",
+    ("total": locations.length)
+  );
+}
+const geolocations = Geolocations.find().fetch();
+if (Contexts.find().count() == 0) {
+  const insertContext = {
+    name: "sao paolo 2018",
+    geolocations: _.pluck(geolocations, "_id")
+  };
+  Contexts.insert(insertContext);
+  logger.debug("fixtures. created first context");
+}
+
+const context = Contexts.findOne();
+const audiences = [
+  {
+    title: "Anarchism",
+    spec: { interests: ["6003029947985"] },
+    contextIds: [context._id]
+  },
+  {
+    title: "Environmentalism",
+    spec: { interests: ["6003970975896"] },
+    contextIds: [context._id]
+  },
+  {
+    title: "Security",
+    spec: { interests: ["6003345669874"] },
+    contextIds: [context._id]
+  },
+  {
+    title: "Politics and social issues",
+    spec: { interests: ["6011515350975"] },
+    contextIds: [context._id]
+  },
+  {
+    title: "Public transport",
+    spec: { interests: ["6003387449393"] },
+    contextIds: [context._id]
+  }
+];
+
+if (AudienceCategories.find().count() == 0) {
   for (const category of audiences) {
-    AudiencesCategories.insert(category);
+    AudienceCategories.insert(category);
   }
   logger.debug(
     "fixtures. audiences categories added:",
