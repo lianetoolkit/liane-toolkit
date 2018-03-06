f19b8102a1c8c181a1de85bc47778e3aeb86cc50
miguelpeixe <miguel@cardume.art.br>
2018-03-06 11:11:53 -0300

    better repeater field ux

--- imports/ui/components/canvas/RepeaterField.jsx
+++ imports/ui/components/canvas/RepeaterField.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Header, Label, Button } from "semantic-ui-react";
+import { Header, Label, Button, Icon, Divider } from "semantic-ui-react";
 import CanvasField from "./CanvasField.jsx";
 import styled from "styled-components";
 import { set } from "lodash";
@@ -19,14 +19,22 @@ export default class RepeaterField extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: []
+      value: [],
+      activeIndex: 0
     };
     this._new = this._new.bind(this);
     this._handleChange = this._handleChange.bind(this);
+    this._handlePrevClick = this._handlePrevClick.bind(this);
+    this._handleNextClick = this._handleNextClick.bind(this);
   }
   componentDidMount() {
-    if (this.props.value) {
-      this.setState({ value: this.props.value });
+    const { value } = this.props;
+    if (value) {
+      this.setState({ value });
+    } else {
+      if (!value || !value.length) {
+        this._new();
+      }
     }
   }
   componentWillReceiveProps(nextProps) {
@@ -34,12 +42,18 @@ export default class RepeaterField extends React.Component {
     if (JSON.stringify(value) !== JSON.stringify(nextProps.value)) {
       this.setState({ value: nextProps.value });
     }
+    if (!this.state.value || !this.state.value.length) {
+      this._new();
+    }
   }
   componentDidUpdate(prevProps, prevState) {
     const { onChange, config } = this.props;
     const { value } = this.state;
     if (JSON.stringify(prevState.value) !== JSON.stringify(value)) {
       if (onChange) onChange(null, { name: config.key, value: value });
+    }
+    if (!value || !value.length) {
+      this._new();
     }
   }
   _handleChange = (e, { name, value }) => {
@@ -50,47 +64,81 @@ export default class RepeaterField extends React.Component {
     });
   };
   _new(ev) {
-    ev.preventDefault();
+    if (ev) {
+      ev.preventDefault();
+    }
     const { config } = this.props;
     const { value } = this.state;
     if (!config.limit || value.length < config.limit) {
       this.setState({
-        value: [...value, {}]
+        value: [...value, {}],
+        activeIndex: value.length
       });
     }
   }
-  _item(value, i) {
+  _handlePrevClick(ev) {
+    if (ev) ev.preventDefault();
+    const { activeIndex } = this.state;
+    if (activeIndex > 0) {
+      this.setState({
+        activeIndex: activeIndex - 1
+      });
+    }
+  }
+  _handleNextClick(ev) {
+    if (ev) ev.preventDefault();
+    const { activeIndex, value } = this.state;
+    if (activeIndex < value.length - 1) {
+      this.setState({
+        activeIndex: activeIndex + 1
+      });
+    }
+  }
+  _item(i) {
+    const { value } = this.state;
     const { config } = this.props;
-    return (
-      <Item key={i}>
-        {config.fields.map(field => (
-          <CanvasField
-            key={field.key}
-            config={{ ...field, key: `[${i}]${field.key}` }}
-            onChange={this._handleChange}
-            value={value[field.key]}
-          />
-        ))}
-      </Item>
-    );
+    if (value.length) {
+      const itemValue = value[i];
+      return (
+        <Item key={i}>
+          {config.fields.map(field => (
+            <CanvasField
+              key={field.key}
+              config={{ ...field, key: `[${i}]${field.key}` }}
+              onChange={this._handleChange}
+              value={itemValue[field.key]}
+            />
+          ))}
+        </Item>
+      );
+    }
+    return null;
   }
   render() {
+    const { value, activeIndex } = this.state;
     const { config } = this.props;
-    const { value } = this.state;
     return (
       <div>
-        <Button size="small" floated="right" onClick={this._new}>
+        <Header size="small" floated="left">
+          {config.label}
+        </Header>
+        <Button basic size="small" floated="right" onClick={this._new}>
           + Add new
         </Button>
-        <Header size="small">
-          {config.label}
-          {config.limit ? (
-            <Label>
-              {value.length}/{config.limit}
-            </Label>
-          ) : null}
-        </Header>
-        {value.map((item, i) => this._item(item, i))}
+        <Button.Group size="mini" basic>
+          <Button icon="chevron left" onClick={this._handlePrevClick} />
+          <Button icon="chevron right" onClick={this._handleNextClick} />
+        </Button.Group>{" "}
+        <Label basic size="tiny">
+          {activeIndex + 1}/{value.length}
+        </Label>{" "}
+        <Label basic size="tiny">
+          {config.limit ? <span>max: {config.limit}</span> : null}
+        </Label>
+        <Divider hidden />
+        {this._item(activeIndex)}
+        <Divider hidden />
+        {/* {value.map((item, i) => this._item(item, i))} */}
       </div>
     );
   }
