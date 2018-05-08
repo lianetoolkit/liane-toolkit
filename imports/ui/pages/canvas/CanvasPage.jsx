a8c499a7dfac8c5efa12a5ef71cd06929211fc58
miguelpeixe <miguel@cardume.art.br>
2018-05-08 11:29:37 -0300

    support canvas server for print

--- imports/ui/pages/canvas/CanvasPage.jsx
+++ imports/ui/pages/canvas/CanvasPage.jsx
@@ -1,10 +1,13 @@
+import axios from "axios";
 import React from "react";
 import PageHeader from "/imports/ui/components/app/PageHeader.jsx";
 import Loading from "/imports/ui/components/utils/Loading.jsx";
-import Alerts from "/imports/ui/utils/Alerts.js";
+import { Alerts } from "/imports/ui/utils/Alerts.js";
 import styled from "styled-components";
 import CanvasModel from "/imports/api/canvas/model/canvas";
 import FlexDataItem from "/imports/ui/components/flexData/FlexDataItem.jsx";
+
+const canvasUrl = Meteor.settings.public.canvasUrl;
 
 import {
   Segment,
@@ -43,7 +46,10 @@ const CanvasItems = styled.div`
 export default class CanvasEdit extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      exporting: false
+    };
+    this._handleExport = this._handleExport.bind(this);
   }
   componentDidMount() {}
   _fieldData(section, field) {
@@ -53,7 +59,39 @@ export default class CanvasEdit extends React.Component {
         fieldData.sectionKey == section.key && fieldData.key == field.key
     );
   }
+  _handleExport(ev) {
+    ev.preventDefault();
+    const { campaign } = this.props;
+    this.setState({ exporting: true });
+    Meteor.call(
+      "canvas.getNormalized",
+      { campaignId: campaign._id },
+      (err, data) => {
+        axios
+          .post(`${canvasUrl}/api`, {
+            canvas: data,
+            json: true,
+            fromLiane: true
+          })
+          .then(res => {
+            this.setState({ exporting: false });
+            if (res.data.id) {
+              window.open(`${canvasUrl}/${res.data.id}`);
+            } else {
+              Alerts.error("Unexpected response from canvas server.");
+              // error
+            }
+          })
+          .catch(err => {
+            Alerts.error(err);
+            this.setState({ exporting: false });
+            console.log(err);
+          });
+      }
+    );
+  }
   render() {
+    const { exporting } = this.state;
     const { loading, campaign, canvas } = this.props;
     return (
       <div>
@@ -65,6 +103,17 @@ export default class CanvasEdit extends React.Component {
           subTitle="Canvas"
         />
         <section className="content">
+          {canvasUrl ? (
+            <div>
+              <Button.Group basic floated="right">
+                <Button icon onClick={this._handleExport} disabled={exporting}>
+                  <Icon name={exporting ? "spinner" : "print"} />
+                  Export canvas
+                </Button>
+              </Button.Group>
+              <Divider hidden clearing />
+            </div>
+          ) : null}
           {loading ? (
             <Loading />
           ) : (
