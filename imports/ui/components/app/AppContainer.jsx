4511697a1d5afa6befc61bb8a4fb563a21efc91e
Miguel Peixe <miguel@cardume.art.br>
2019-01-21 17:14:25 -0200

    update meteor and start grommet implementation

--- imports/ui/components/app/AppContainer.jsx
+++ imports/ui/components/app/AppContainer.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+// import {
+//   Box,
+//   Button,
+//   Collapsible,
+//   Heading,
+//   Grommet,
+//   Layer,
+//   ResponsiveContext
+// } from "grommet";
+
 import styled from "styled-components";
 
 import "leaflet/dist/leaflet.css";
+
+const theme = {
+  global: {
+    colors: {
+      brand: "#228BE6"
+    },
+    font: {
+      family: "Roboto",
+      size: "14px",
+      height: "20px"
+    }
+  }
+};
+
+// export default class AppContainer extends React.Component {
+//   render() {
+//     return (
+//       <Grommet theme={theme} full>
+//         <ResponsiveContext.Consumer>
+//           {size => <div>{this.props.children}</div>}
+//         </ResponsiveContext.Consumer>
+//       </Grommet>
+//     );
+//   }
+// }
 
 export default styled.div`
   display: flex;
