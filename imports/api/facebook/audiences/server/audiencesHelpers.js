36bc96c10f717e9b222988be76c308c71497ce8c
Miguel Peixe <miguel@cardume.art.br>
2019-01-29 13:08:12 -0200

    turn config into helper

--- imports/api/facebook/audiences/server/audiencesHelpers.js
+++ imports/api/facebook/audiences/server/audiencesHelpers.js
@@ -15,7 +15,7 @@ import redisClient, { deleteByPattern } from "/imports/startup/server/redis";
 import crypto from "crypto";
 import _ from "underscore";
 import moment from "moment";
-import demographicsConfig from "./demographicsConfig.js";
+import { FacebookDemographicsHelpers } from "./demographicsHelpers.js";
 
 const FacebookAudiencesHelpers = {
   async getFanCount(facebookId, token) {
@@ -215,14 +215,7 @@ const FacebookAudiencesHelpers = {
     };
   },
   _buildDemographics() {
-    // const keys = Object.keys(demographicsConfig.config);
-    // let specs = [];
-    // for(let key of keys) {
-    //   // demographicsConfig[key].map
-    // }
-    // console.log(demographicsConfig.specs);
-    // return Object.getOwnPropertyNames(demographicsConfig.specs);
-    const specs = demographicsConfig.specs;
+    const specs = FacebookDemographicsHelpers.build();
     console.log(specs);
     return specs;
   },
