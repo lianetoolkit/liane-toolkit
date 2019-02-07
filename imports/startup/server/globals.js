2fdd442db9521d3516997675642dbdb15fd47fbf
Miguel Peixe <miguel@cardume.art.br>
2019-02-07 15:37:04 -0200

    update FB API version

--- imports/startup/server/globals.js
+++ imports/startup/server/globals.js
@@ -1,7 +1,7 @@
 import { Facebook, FacebookApiException } from "fb";
 
 FB = new Facebook({
-  version: "v3.0",
+  version: "v3.2",
   client_id: Meteor.settings.facebook.clientId,
   client_secret: Meteor.settings.facebook.clientSecret
 });
