d0c10a73952a6561353c5b7d8fc6442af491035e
Miguel Peixe <miguel@peixe.co>
2019-04-12 16:51:47 -0300

    start AddressField component

---
+++ imports/ui2/components/AddressField.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+export default class AddressField extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return null;
+  }
+}
