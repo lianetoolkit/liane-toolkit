12322e5d99d26ab907afe5c245cc83048b489716
miguelpeixe <miguel@cardume.art.br>
2019-08-13 12:41:43 -0300

    OrLine component

---
+++ imports/ui2/components/OrLine.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 1px;
+  border-bottom: 1px solid #eee
+  margin: 1rem 0;
+  .or-text {
+    font-size: 0.8em;
+    color: #999;
+    flex: 0 0 auto;
+    background: #fff;
+    padding: 0 1rem;
+  }
+`;
+
+export default class OrLine extends Component {
+  render() {
+    const text = this.props.children || "or";
+    return (
+      <Container>
+        <span className="or-text">{text}</span>
+      </Container>
+    );
+  }
+}
