3604369998283370a31d373fad85251be976f79f
Miguel Peixe <miguel@peixe.co>
2019-09-02 16:51:10 -0300

    support GET method for validation

--- imports/api/facebook/people/server/peopleRest.js
+++ imports/api/facebook/people/server/peopleRest.js
@@ -17,9 +17,6 @@ Picker.middleware(bodyParser.json());
 Picker.middleware(bodyParser.urlencoded({ extended: false }));
 
 Picker.route("/subscription/:pid/people", (params, req, res, next) => {
-  if (req.method !== "POST") {
-    return sendError(405, "Method not allowed", res);
-  }
   if (!token || !params.query.token || params.query.token !== token) {
     return sendError(401, "Invalid token", res);
   }
@@ -36,68 +33,76 @@ Picker.route("/subscription/:pid/people", (params, req, res, next) => {
   if (!campaign) {
     return sendError(404, "No campaign found for this page", res);
   }
-  const body = req.body;
-  const data = body.data || {};
-  if (body.uid) {
-    if (!People.findOne({ facebookId: body.uid }) && !data.name) {
-      return sendError(
-        404,
-        "Person not found. Name is required for creating new person entry",
-        res
-      );
-    }
-    let $set = {};
-    let $unset = {};
-    let $setOnInsert = {
-      campaignId: campaign._id,
-      facebookAccountId: facebookAccount.facebookId
-    };
-    if (data.hasOwnProperty("name")) {
-      $setOnInsert["name"] = data.name;
-    }
-    if (data.hasOwnProperty("donor")) {
-      $set["campaignMeta.donor"] = data.donor;
-    }
-    if (data.hasOwnProperty("volunteer")) {
-      $set["campaignMeta.volunteer"] = data.volunteer;
-    }
-    if (data.hasOwnProperty("email")) {
-      $set["campaignMeta.contact.email"] = data.email;
-    }
-    if (data.hasOwnProperty("phone")) {
-      $set["campaignMeta.contact.cellphone"] = data.phone;
-    }
-    if (data.hasOwnProperty("conversationId")) {
-      $set["campaignMeta.chatbotConversationId"] = data.conversationId;
-    }
-    if (body.hasOwnProperty("status")) {
-      if (!body.status || body.status == null || body.status == undefined) {
-        $unset["chatbotStatus"] = "";
-      } else {
-        $set["chatbotStatus"] = body.status;
+  if (req.method == "POST") {
+    const body = req.body;
+    const data = body.data || {};
+    if (body.uid) {
+      if (!People.findOne({ facebookId: body.uid }) && !data.name) {
+        return sendError(
+          404,
+          "Person not found. Name is required for creating new person entry",
+          res
+        );
+      }
+      let $set = {};
+      let $unset = {};
+      let $setOnInsert = {
+        campaignId: campaign._id,
+        facebookAccountId: facebookAccount.facebookId
+      };
+      if (data.hasOwnProperty("name")) {
+        $setOnInsert["name"] = data.name;
+      }
+      if (data.hasOwnProperty("donor")) {
+        $set["campaignMeta.donor"] = data.donor;
+      }
+      if (data.hasOwnProperty("volunteer")) {
+        $set["campaignMeta.volunteer"] = data.volunteer;
+      }
+      if (data.hasOwnProperty("email")) {
+        $set["campaignMeta.contact.email"] = data.email;
+      }
+      if (data.hasOwnProperty("phone")) {
+        $set["campaignMeta.contact.cellphone"] = data.phone;
+      }
+      if (data.hasOwnProperty("conversationId")) {
+        $set["campaignMeta.chatbotConversationId"] = data.conversationId;
+      }
+      if (body.hasOwnProperty("status")) {
+        if (!body.status || body.status == null || body.status == undefined) {
+          $unset["chatbotStatus"] = "";
+        } else {
+          $set["chatbotStatus"] = body.status;
+        }
+      }
+      const modifier = { $setOnInsert, $set, $unset };
+      try {
+        People.upsert(
+          {
+            campaignId: campaign._id,
+            facebookAccountId: facebookAccount.facebookId,
+            facebookId: body.uid
+          },
+          modifier
+        );
+      } catch (err) {
+        return sendError(
+          err.sanitizedError.error,
+          err.sanitizedError.reason,
+          res
+        );
+      } finally {
+        res.statusCode = 201;
+        res.end();
+        return;
       }
     }
-    const modifier = { $setOnInsert, $set, $unset };
-    try {
-      People.upsert(
-        {
-          campaignId: campaign._id,
-          facebookAccountId: facebookAccount.facebookId,
-          facebookId: body.uid
-        },
-        modifier
-      );
-    } catch (err) {
-      return sendError(
-        err.sanitizedError.error,
-        err.sanitizedError.reason,
-        res
-      );
-    } finally {
-      res.statusCode = 201;
-      res.end();
-      return;
-    }
+  } else if (req.method == "GET") {
+    res.statusCode = 200;
+    res.end();
+    return;
+  } else {
+    return sendError(405, "Method not allowed", res);
   }
   return sendError(400, "Bad request", res);
 });
