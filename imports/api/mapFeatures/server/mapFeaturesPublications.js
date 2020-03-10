a2c6e3f7cd19339722553ec3b6b9098014fde3f2
Miguel Peixe <miguel@peixe.co>
2020-03-10 11:16:43 -0300

    integrate permissions to map features

--- imports/api/mapFeatures/server/mapFeaturesPublications.js
+++ imports/api/mapFeatures/server/mapFeaturesPublications.js
@@ -4,7 +4,14 @@ import { Campaigns } from "/imports/api/campaigns/campaigns";
 Meteor.publish("mapFeatures.byCampaign", function({ campaignId }) {
   this.unblock();
   const userId = this.userId;
-  if (Meteor.call("campaigns.canManage", { campaignId, userId })) {
+  if (
+    Meteor.call("campaigns.userCan", {
+      campaignId,
+      userId,
+      feature: "map",
+      permission: "view"
+    })
+  ) {
     return MapFeatures.find({ campaignId }, { sort: { createdAt: -1 } });
   }
   return this.ready();
