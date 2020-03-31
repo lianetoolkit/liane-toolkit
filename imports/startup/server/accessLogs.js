ab170b87ce8a6dff965677202dd9bd0fa4596e1c
Miguel Peixe <miguel@peixe.co>
2020-03-31 11:30:31 -0300

    do not fetch country data for local ips

--- imports/startup/server/accessLogs.js
+++ imports/startup/server/accessLogs.js
@@ -3,15 +3,21 @@ import { AccessLogs } from "/imports/api/accessLogs/accessLogs";
 import axios from "axios";
 
 // if (Meteor.settings.public.deployMode !== "local") {
-  Accounts.onLogin(function(data) {
-    const ip = data.connection.clientAddress;
-    let insertDoc = {
-      ip,
-      type: "open",
-      userId: data.user._id,
-      connectionId: data.connection.id
-    };
-    let geoData = false;
+Accounts.onLogin(function(data) {
+  const ip = data.connection.clientAddress;
+  let insertDoc = {
+    ip,
+    type: "open",
+    userId: data.user._id,
+    connectionId: data.connection.id
+  };
+  let geoData = false;
+  const localIpRegexp = new RegExp(
+    /(^127\.)|(^192\.168\.)|(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^::1$)|(^[fF][cCdD])/
+  );
+  if (localIpRegexp.test(ip)) {
+    AccessLogs.insert(insertDoc);
+  } else {
     axios
       .get(`https://get.geojs.io/v1/country/${ip}.json`)
       .then(res => {
@@ -21,22 +27,23 @@ import axios from "axios";
         if (geoData && geoData.country) insertDoc.country = geoData.country;
         AccessLogs.insert(insertDoc);
       });
-  });
+  }
+});
 
-  Meteor.onConnection(function(connection) {
-    connection.onClose(function(data) {
-      const lastLog = AccessLogs.findOne({
-        connectionId: connection.id,
-        type: "view"
-      });
-      if (lastLog) {
-        AccessLogs.insert({
-          type: "close",
-          ip: lastLog.ip,
-          userId: lastLog.userId,
-          connectionId: connection.id
-        });
-      }
+Meteor.onConnection(function(connection) {
+  connection.onClose(function(data) {
+    const lastLog = AccessLogs.findOne({
+      connectionId: connection.id,
+      type: "view"
     });
+    if (lastLog) {
+      AccessLogs.insert({
+        type: "close",
+        ip: lastLog.ip,
+        userId: lastLog.userId,
+        connectionId: connection.id
+      });
+    }
   });
+});
 // }
