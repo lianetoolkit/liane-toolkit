6059ba5d93733daee30b05b3e22043d0d649f1e5
Marcos Javier Riganti <marcos.riganti@gmail.com>
2020-08-14 17:08:55 -0300

    DSB-127 - addding basic JS files

---
+++ imports/api/dashboard/server/dashboardMethods.js
@@ -0,0 +1,64 @@
+import SimpleSchema from "simpl-schema";
+import moment from "moment";
+
+
+export const summaryData = new ValidatedMethod({
+    name: "dashboard.summary",
+    validate: new SimpleSchema({
+        campaignId: {
+            type: String,
+        },
+    }).validator(),
+    run({ campaignId }) {
+
+        logger.debug("dashboard.summary", { campaignId });
+        const userId = Meteor.userId();
+
+        //TODO Define permission  feature
+
+        if (
+            !Meteor.call("campaigns.userCan", {
+                campaignId,
+                userId,
+                feature: "dashboard",
+                permission: "view",
+            })
+        ) {
+            throw new Meteor.Error(401, "You are not allowed to do this action");
+        }
+
+        // Queries
+
+        // return 
+    },
+});
+export const achievements = new ValidatedMethod({
+    name: "dashboard.achievements",
+    validate: new SimpleSchema({
+        campaignId: {
+            type: String,
+        },
+    }).validator(),
+    run({ campaignId }) {
+
+        logger.debug("dashboard.summary", { campaignId });
+        const userId = Meteor.userId();
+
+        //TODO Define permission feature
+        if (
+            !Meteor.call("campaigns.userCan", {
+                campaignId,
+                userId,
+                feature: "dashboard",
+                permission: "view",
+            })
+        ) {
+            throw new Meteor.Error(401, "You are not allowed to do this action");
+        }
+        // Queries
+
+        // return 
+    },
+});
+
+
