51a1b33879ff79842eb91f89f508e213f60e4c3a
Miguel Peixe <miguel@peixe.co>
2020-08-20 12:48:53 -0300

    fix notification pub selector

--- imports/api/notifications/server/notificationsPublications.js
+++ imports/api/notifications/server/notificationsPublications.js
@@ -1,20 +1,13 @@
 import { Notifications } from "/imports/api/notifications/notifications.js";
 import { Campaigns } from "/imports/api/campaigns/campaigns.js";
 
-Meteor.publish("notifications.byUser", function() {
+Meteor.publish("notifications.byUser", function () {
   const currentUser = this.userId;
-  let selector = {
-    $or: [{ userId: currentUser }]
-  };
-  const userCampaigns = Campaigns.find({
-    users: { $elemMatch: { userId: currentUser } }
-  }).fetch();
-  if (userCampaigns) {
-    const campaignsIds = userCampaigns.map(campaign => campaign._id);
-    selector.$or.push({ campaignId: { $in: campaignsIds } });
-  }
   if (currentUser) {
-    return Notifications.find(selector, { sort: { createdAt: -1 } });
+    return Notifications.find(
+      { userId: currentUser },
+      { sort: { createdAt: -1 } }
+    );
   } else {
     return this.ready();
   }
