49153e45f427c8fcec033220bbe002524128a932
Marcos Javier Riganti <marcos.riganti@gmail.com>
2020-08-24 12:04:23 +0200

    DSB-127 - adding test information

--- imports/ui2/pages/DashboardDemo.jsx
+++ imports/ui2/pages/DashboardDemo.jsx
@@ -1,26 +1,43 @@
 import React from "react";
 import Page from "/imports/ui2/components/Page.jsx";
 import Loading from "/imports/ui2/components/Loading.jsx";
+// import { summaryData } from '../../api/dashboard/server/dashboardMethods';
 
 class DashboardDemoPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       loading: false,
+      summaryData: null
     };
   }
   componentDidMount() {
     // Call methods here
+    const { campaign } = this.props;
+    Meteor.call("dashboard.summary", { campaignId: campaign._id }, (err, data) => {
+      if (err) {
+        console.log("dashboard.summary error ", err)
+        this.setState({
+          loading: false,
+        });
+      } else {
+        console.log("dashboard.summary getting summaryData ", data)
+        this.setState({ summaryData: data });
+      }
+    });
+
   }
   render() {
     const { campaign } = this.props;
-    const { loading } = this.state;
+    const { loading, summaryData } = this.state;
     if (loading) {
       return <Loading full />;
     }
     return (
       <Page.Content>
         <h2>{campaign.name} Demo Dashboard</h2>
+        <pre>ID:{JSON.stringify(campaign._id)}</pre>
+        <pre>summaryData: {JSON.stringify(summaryData)}</pre>
       </Page.Content>
     );
   }
