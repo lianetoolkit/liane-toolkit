04053f0e8d48a16f31042cd35a2fbdd77a90225c
Marcos Javier Riganti <marcos.riganti@gmail.com>
2020-08-25 21:30:09 +0200

    DSB-127  - adding likes queriess

--- imports/api/dashboard/server/dashboardMethods.js
+++ imports/api/dashboard/server/dashboardMethods.js
@@ -7,6 +7,9 @@ import {
 } from "/imports/api/facebook/people/people.js";
 import { Likes } from "/imports/api/facebook/likes/likes.js";
 
+// import redisClient, { deleteByPattern } from "/imports/startup/server/redis";
+
+
 export const summaryData = new ValidatedMethod({
     name: "dashboard.summary",
     validate: new SimpleSchema({
@@ -24,8 +27,7 @@ export const summaryData = new ValidatedMethod({
         const facebookAccountId = campaign.facebookAccount.facebookId;
 
 
-
-        //TODO Define permission  feature
+        //? TODO Define permission  feature
         if (
             !Meteor.call("campaigns.userCan", {
                 campaignId,
@@ -36,7 +38,6 @@ export const summaryData = new ValidatedMethod({
         ) {
             throw new Meteor.Error(401, "You are not allowed to do this action");
         }
-        // Queries
         // 1 -  Total people in people directory
         const toCount = People.find({
             campaignId
@@ -45,9 +46,11 @@ export const summaryData = new ValidatedMethod({
             toCount.count()
         )
         // 2 - Total positive reactions(like, love and wow)
-        // search for positive reaactions by facebookaccountId
         let positiveReactions = 0;
-
+        positiveReactions = Promise.await(Likes.find({
+            facebookAccountId,
+            "type": { "$in": ['LIKE', 'CARE', 'PRIDE', 'LOVE', 'WOW'] }
+        }).count())
 
         // 3 - Total comments
         let comments = 0;
@@ -65,13 +68,14 @@ export const summaryData = new ValidatedMethod({
             toPM.count()
         )
         // redis update
-
-        return {
+        const result = {
             totalPeople,
             positiveReactions,
             comments,
             peoplePM
-        }
+        };
+
+        return result
         // return 
     },
 });
