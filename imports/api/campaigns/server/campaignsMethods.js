3c713a7e3dfaa079fc9ef751aadbdcad65ac6249
Miguel Peixe <miguel@peixe.co>
2020-11-18 14:02:20 -0300

    support multiple campaign selection for message system

--- imports/api/campaigns/server/campaignsMethods.js
+++ imports/api/campaigns/server/campaignsMethods.js
@@ -390,13 +390,16 @@ export const campaignValidateInvite = new ValidatedMethod({
 export const campaignsSelectGet = new ValidatedMethod({
   name: "campaigns.selectGet",
   validate: new SimpleSchema({
-    campaignId: {
+    campaignIds: {
+      type: Array,
+    },
+    "campaignIds.$": {
       type: String,
     },
   }).validator(),
-  run({ campaignId }) {
+  run({ campaignIds }) {
     this.unblock();
-    logger.debug("campaigns.selectGet called", { campaignId });
+    logger.debug("campaigns.selectGet called", { campaignIds });
 
     const userId = Meteor.userId();
     if (!userId) {
@@ -407,7 +410,7 @@ export const campaignsSelectGet = new ValidatedMethod({
       throw new Meteor.Error(401, "You are not allowed to do this action");
     }
 
-    let selector = { _id: campaignId };
+    let selector = { _id: { $in: campaignIds } };
     let options = {
       fields: {
         name: 1,
